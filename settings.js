68032ee2a37ca882a01f2d133bde56edf51f73c8
Sedetil <115119502+Sedetil@users.noreply.github.com>
2023-06-06 19:03:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Wanz-BOT'// yourname
-global.MONGO_DB_URI = "mongodb+srv://mongodb-bot:87yEBsinWxcQikKh@cluster0.hli6aku.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://mongodb-bot:ICz6nIvZbrW9s2ml@bluster0.rykllph.mongodb.net/" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "babayohnblack.com" //email
 global.email_password = "iyzs jwal rxtr yztr" //application password email
